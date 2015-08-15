05fdc3cce252922776fb61ed1b2548bd2b7bb13a
nerv <nervgh@gmail.com>
2015-08-15 19:10:59 +0300

    Added: draw pointer

--- sandbox/binarizaciya-rukopisnogo-labirinta/dist/app.js
+++ sandbox/binarizaciya-rukopisnogo-labirinta/dist/app.js
@@ -59,7 +59,17 @@
 	
 	decoder.onDecodeComplete = function (matrix, pointerSize) {
 	    console.log("pointerSize", pointerSize);
+	
 	    decoder.writer.writeAsCanvas(matrix, stage);
+	
+	    var context = stage.getContext("2d");
+	    context.beginPath();
+	    context.arc(46, 18, pointerSize >> 1, 0, 2 * Math.PI);
+	    context.strokeStyle = "rgb(255,0,255)";
+	    context.stroke();
+	    context.fillStyle = "rgb(255,0,255)";
+	    context.fill();
+	    context.closePath();
 	};
 	
 	decoder.decode(image);
--- sandbox/binarizaciya-rukopisnogo-labirinta/src/index.js
+++ sandbox/binarizaciya-rukopisnogo-labirinta/src/index.js
@@ -12,7 +12,17 @@ let decoder = new Decoder();
 
 decoder.onDecodeComplete = function(matrix, pointerSize) {
     console.log('pointerSize', pointerSize);
+
     decoder.writer.writeAsCanvas(matrix, stage);
+
+    let context = stage.getContext('2d');
+    context.beginPath();
+    context.arc(46, 18, pointerSize >> 1, 0, 2 * Math.PI);
+    context.strokeStyle = 'rgb(255,0,255)';
+    context.stroke();
+    context.fillStyle = 'rgb(255,0,255)';
+    context.fill();
+    context.closePath();
 };
 
 decoder.decode(image);
